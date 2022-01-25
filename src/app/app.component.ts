30757a7be78032a2def9a8d30a185f11d93e2c94
Debin Jose <dj5449@srmist.edu.in>
2022-01-25 11:55:09 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
+const config = {
+  apiKey: 'AIzaSyB2sU74tWxMf_9GTEX18slF7hjd1wMjk-0',
+  databaseURL: "https://angularchat-3d1a9-default-rtdb.firebaseio.com"
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-chat';
+  constructor() {
+    firebase.initializeApp(config);
+  }
+  
 }
